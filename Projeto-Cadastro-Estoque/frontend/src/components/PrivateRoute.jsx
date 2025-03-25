b11c6af2395978fcf485b5fd31004f29c800f053
Alexsander Ribeiro <62142077+alexsanderneto@users.noreply.github.com>
2025-03-25 08:51:34 -0300

    Delete Projeto-Cadastro-Estoque directory

--- Projeto-Cadastro-Estoque/frontend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const isAuthenticated = !!localStorage.getItem("token"); // Verifica se há um token armazenado
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
